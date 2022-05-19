a036381363f8527f17011868a544806a74b3ec98
omertov <51419961+omertov@users.noreply.github.com>
2022-05-19 21:43:02 +0300

    Compute latent_avg if not provided within the checkpoint.

--- models/psp.py
+++ models/psp.py
@@ -94,7 +94,11 @@ class pSp(nn.Module):
     def __load_latent_avg(self, ckpt, repeat=None):
         if 'latent_avg' in ckpt:
             self.latent_avg = ckpt['latent_avg'].to(self.opts.device)
-            if repeat is not None:
-                self.latent_avg = self.latent_avg.repeat(repeat, 1)
+        elif self.opts.start_from_latent_avg:
+            # Compute mean code based on a large number of latents (10,000 here)
+            with torch.no_grad():
+                self.latent_avg = self.decoder.mean_latent(10000)
         else:
             self.latent_avg = None
+        if repeat is not None and self.latent_avg is not None:
+            self.latent_avg = self.latent_avg.repeat(repeat, 1)
